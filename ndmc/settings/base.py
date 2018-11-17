4d1d15b951348df7c030a07c7eb7c0315c43001b
cabilangan112 <jassencbilangan@gmail.com>
2018-11-17 15:13:20 +0800

    migrate app

--- ndmc/settings/base.py
+++ ndmc/settings/base.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'social_django',
     'account',
     'alutracer',
-    'pdfdrive'
+    'pdfdrive',
  
 ]
 
--- ndmc/settings/local.py
+++ ndmc/settings/local.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'alutracer',
-    'pdfdrive'
+    'pdfdrive',
     'social_django',
 ]
 
