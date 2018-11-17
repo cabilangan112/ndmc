6ecfc56fd4acf4203dc9f9e887dbb71a8c2bc1b1
cabilangan112 <jassencbilangan@gmail.com>
2018-11-17 14:23:14 +0800

    file validators

--- pdfdrive/models.py
+++ pdfdrive/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.conf import settings
+from .validators import validate_file_extention
+from django.urls import reverse
+ 
 
-# Create your models here.
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
+class PathAndRename(object):
+
+    def __init__(self,sub_path):
+        self.path = sub_path
+
+    def __call__(self,instance, filename):
+        ext = filename.split('.')[-1]
+
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk,ext)
+        else:
+            filename = '{}.{}'.format(instance.pk,ext)
+        return os.path.join(self.path, filename)
+path_and_rename = PathAndRename("file/")
+
