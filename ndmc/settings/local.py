1dea5a59ee815f3986efadef360b9096aa219489
cabilangan112 <jassencbilangan@gmail.com>
2018-11-19 17:17:28 +0800

    models Post, Author, course department

--- ndmc/settings/local.py
+++ ndmc/settings/local.py
@@ -37,11 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
- 
-    'social_django',
     'account',
     'alutracer',
     'pdfdrive',
+    'social_django',
  
 ]
 
--- pdfdrive/models.py
+++ pdfdrive/models.py
@@ -3,7 +3,7 @@ from django.core.validators import RegexValidator
 from django.conf import settings
 from .validators import validate_file_extention
 from django.urls import reverse
- 
+User = settings.AUTH_USER_MODEL
 
 def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
@@ -23,3 +23,55 @@ class PathAndRename(object):
         return os.path.join(self.path, filename)
 path_and_rename = PathAndRename("media/file/")
 
+YEAR = (
+    ('1st', '1st'),
+    ('2nd', '2nd'),
+    ('3rd', '3rd'),
+    ('4rt', '4rt'),
+)
+
+class Course(models.Model):
+    course_code        = models.CharField(max_length=100)
+    course_description = models.CharField(max_length=100) 
+    date_created       = models.DateTimeField(auto_now_add=True)
+    date_modified      = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return '{}'.format(self.course_code)
+
+class Department(models.Model):
+    department_code        = models.CharField(max_length=100)
+    department_description = models.CharField(max_length=100) 
+    date_created           = models.DateTimeField(auto_now_add=True)
+    date_modified          = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return '{}'.format(self.department_code)
+
+class Author(models.Model):
+    lastname   = models.CharField(max_length=100)
+    firtsname  = models.CharField(max_length=100)
+    middlename = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, null=True,on_delete=models.CASCADE)
+    course     = models.ForeignKey(Course, null=True,on_delete=models.CASCADE)
+    Year       = models.CharField(max_length=6, choices=YEAR, blank=True, default=True)
+
+    def __str__(self):
+        return '{}'.format(self.lastname, firtsname)
+
+class Post(models.Model):
+    title          = models.CharField(max_length=100)
+    description    = models.TextField(null=True, blank=True)
+    author         = models.CharField(max_length=100)
+    file           = models.FileField(upload_to="media/pdf", null=True)
+    date_uploaded  = models.DateTimeField(auto_now_add=True)
+    modified       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+
+
+ 
