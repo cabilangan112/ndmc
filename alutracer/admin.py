ee4c8195c9ff1f079e3085aeca0881cce9510211
siuols <louisaleksieb.dagusen@gmail.com>
2018-11-21 09:33:53 +0800

    Admin TabularInline Create choices inline for question

--- alutracer/admin.py
+++ alutracer/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Question,Course, Choice,PersonalInformation
+from .models import Course, Choice, PersonalInformation, Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoceInline(admin.TabulareInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['question_text']}),
+    ]
+    inline = [ChoceInline]
+    list_display = ('question_text', 'date_created', 'date_modified')
+    search_fields = ['question_text']
+    list_filter = ['date_created']
+
 admin.site.register(Course)
+admin.site.register(Choice)
 admin.site.register(PersonalInformation)
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
