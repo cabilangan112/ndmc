1f683682b6d95087ab1675ca91b94342ed60a132
cabilangan112 <jassencbilangan@gmail.com>
2018-11-21 09:36:40 +0800

    urls ~

--- pdfdrive/urls.py
+++ pdfdrive/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
  
 app_name='pdfdrive'
+from .views import pdflistAPI
+ 
 
+pdf_list = pdflistAPI.as_view({
+	'get': 'list',
+	'post':'create'
+})
 urlpatterns = [
-
+	path('list', pdf_list, name='list'),
 	]
--- pdfdrive/views.py
+++ pdfdrive/views.py
@@ -4,13 +4,22 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-
+from .serializers import pdfdriveserializer
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
-class repoAPI(ViewSet):
+class pdflistAPI(ViewSet):
+
     def list(self ,request):
         queryset = Post.objects.all()
         serializer_context = {'request': request,}
         serializer = pdfdriveserializer(queryset, many=True, context=serializer_context)
         return Response(serializer.data)
+
+    def create(self, *args, **kwargs):
+         
+        serializer = pdfdriveserializer(data=self.request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=201)
+        return Response(serializer.errors, status=400)
